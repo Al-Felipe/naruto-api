6c93c54000f58fa072bc367633788754147703dd
Felipe Alves <felipealves3580@gmail.com>
2024-07-25 13:28:23 -0300

    Create characters endpoint

--- src/main/java/com/example/narutoapi/controller/Controller.java
+++ src/main/java/com/example/narutoapi/controller/Controller.java
@@ -1,4 +1,18 @@
 package com.example.narutoapi.controller;
 
+import com.example.narutoapi.service.ApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+    @Autowired
+    ApplicationService applicationService;
+
+
+    @GetMapping("/characters")
+    public String listCharacters(){
+        return applicationService.listAllCharacters();
+    }
 }
---
+++ src/main/java/com/example/narutoapi/service/ApplicationService.java
@@ -0,0 +1,25 @@
+package com.example.narutoapi.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class ApplicationService {
+    public String getApiData(String url){
+        String data = "";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+
+        if(responseEntity.getStatusCode().is2xxSuccessful()){
+            data = responseEntity.getBody();
+        } else {
+            data = "Falha ao obter dados. Código de status: " + responseEntity.getStatusCode();
+        }
+        return data;
+    }
+
+    public String listAllCharacters(){
+        return getApiData("https://dattebayo-api.onrender.com/characters");
+    }
+}
